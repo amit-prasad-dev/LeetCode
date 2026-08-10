1cc3dcd1aa50f72e741f7098a9bcb0a400a6fe08
Amit Prasad <amitprasads1160@gmail.com>
2026-08-10 23:48:23 +0530

    Time: 40 ms (35.25%), Space: 53.9 MB (13.58%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,18 +1,15 @@
 class Solution {
-    boolean[] dp = new boolean[100001];
+    Boolean[] dp = new Boolean[100001];
 
     public boolean winnerSquareGame(int n) {
         if (n == 0) return false;
 
-        if (dp[n]) return dp[n];
+        if (dp[n] != null) return dp[n];
 
         for (int i = 1;  i * i <= n; i++) {
-            if (!winnerSquareGame(n - i * i)) {
-                dp[n] = true;
-                return true;
-            }
+            if (!winnerSquareGame(n - i * i)) 
+                return dp[n] = true;
         }
-        dp[n] = false;
-        return false;
+        return dp[n] = false;
     }
 }
